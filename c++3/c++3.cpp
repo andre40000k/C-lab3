b5787fe9594094f5a4a96c1f3ac1471688894769
Андрей Будяну <andre40000k@gmail.com>
2019-05-06 17:42:50 +0300

    Добавьте файлы проекта.

---
+++ c++3/c++3.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<string>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int proverca = 0;
+	setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char Mass[7] = {'q','w','e','r','e','w','q'} /*= {'q','t','e','r','p','w','q'}*/;
+
+	/*for (int i = 0; i < 7; i++)
+	{
+		cin >> Mass[i];
+	}*/
+
+	cout << endl;
+
+	for (int i = 0; i < 7; i++)
+	{
+		if (Mass[i] == Mass[6 - i])
+		{
+			++proverca;
+		}
+	}
+	if (proverca == 7)
+	{
+		cout << "Массив симетричен";
+	}
+	else
+	{
+		cout << "Массив не симетричен";
+	}
+}
+
